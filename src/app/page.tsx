19a6d5c84483ca37cb39f0f088d1f74223f375de
Tophy <tophyrakotomavo@gmail.com>
2025-07-31 17:38:11 +0300

    chore: convert HomePage to functional component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import { InstallAppChecker } from "./_components/install-app-checker";
 import { Message } from "./_components/message";
 
-export const HomePage = () => {
+export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <InstallAppChecker />
       <Message />
     </div>
   );
-};
-
-export default HomePage;
+}
